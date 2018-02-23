223fa659c2696da798317e9386db7152fa71c401
Aziz Mohamed <abdelaziz.youssef.awaad@gmail.com>
2018-02-23 14:04:55 +1100

    Adding production config

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,57 +1,51 @@
 import React, {PropTypes} from 'react';
+import { bindActionCreators } from 'redux';
 import {connect} from 'react-redux';
+import {browserHistory} from 'react-router';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
-import {browserHistory} from 'react-router';
-import { bindActionCreators } from 'redux';
 
 class CoursesPage extends React.Component {
-    constructor(props, context)
-    {
-        super(props, context);
-        this.state = {
-            course : {title: ""}
-        };
+  constructor(props, context) {
+    super(props, context);
 
-        this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
-    }
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
+  }
 
-    redirectToAddCoursePage() {
-        browserHistory.push('/course');
-      }
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
+  }
 
-    courseRow(course, index){
-        return <div key={index} > {course.title}</div>;
-    }
-    render(){
-        const {courses} = this.props;
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <input type="submit"
+               value="Add Course"
+               className="btn btn-primary"
+               onClick={this.redirectToAddCoursePage}/>
 
-        return (
-            <div>
-                <h1>Courses</h1>
-                <input type="submit"
-                    value="Add Course"
-                    className="btn btn-primary"
-                    onClick={this.redirectToAddCoursePage}/>
-                <CourseList courses = {courses}/>
-            </div>
-        );
-    }
+        <CourseList courses={this.props.courses}/>
+      </div>
+    );
+  }
 }
 
 CoursesPage.propTypes = {
-    courses: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
+  actions: PropTypes.object.isRequired,
+  courses: PropTypes.array.isRequired
 };
 
-function mapStateToProps(state, ownProps){
-    return {courses : state.courses};
+function mapStateToProps(state, ownProps) {
+  return {
+    courses: state.courses
+  };
 }
 
-function mapDispatchToProps(dispatch){
-    return {
-        actions : bindActionCreators(courseActions, dispatch)
-    };
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(courseActions, dispatch)
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {Link, IndexLink} from 'react-router' ;
+import { Link } from 'react-router';
 
 class HomePage extends React.Component {
-    render(){
+    render() {
         return (
-            <div className = "jumbotron">
-                <h1>PluralSight Administration</h1>
-                <p>React, redux and react router in ES6 for ultra responsive web app </p>
-                <Link to="/about" className = "btn btn-primary btn-lg">Learn more</Link>
+            <div className="jumbotron">
+                <h1>Pluralsight Administration</h1>
+                <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
             </div>
         );
     }
 }
+
 export default HomePage;
