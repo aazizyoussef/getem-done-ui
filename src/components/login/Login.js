26d0f04c433ee12f75810ebb89d7c7a9ebbfe3ec
Aziz Mohamed <abdelaziz.youssef.awaad@gmail.com>
2018-02-26 12:43:03 +1100

    Add google client id

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -12,8 +12,8 @@ const responseGoogle = (response) => {
 const Login = () => {
   return (
   <GoogleLogin
-    clientId="644339519782-37v0onsactiehec1sg2ktla0bu3dvbvp.apps.googleusercontent.com"
-    buttonText="Login"
+    clientId="644339519782-6f5bmtkt0022lchhdu2lb8v8t2agqjlj.apps.googleusercontent.com"
+    buttonText="Google Login"
     onSuccess={responseGoogle}
     onFailure={responseGoogle}
   />
